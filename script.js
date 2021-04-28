795c56f8fdd2766c9f42d883dbe77f9c11ed44b0
Agni-Saha <agnisaha337599@gmail.com>
2021-04-28 21:12:21 +0530

    inor changes

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ timeline
   .to(".rock", 10, { y: -250 })
   .to(".girl", 10, { y: -200 }, "-=10")
   .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
-  // .to(".content-title", 10, { top: "-0%" }, "-=10")
-  // .fromTo(".content-design", { opacity: 0.3 }, { opacity: 1, duration: 3 });
+  .to(".content", 10, { top: "0%" }, "-=10");
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
