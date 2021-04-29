d8afcb38d328eb6364d76c845d11b2ce4cbc4a77
Agni-Saha <agnisaha337599@gmail.com>
2021-04-30 01:59:37 +0530

    modified navbar

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .to(".rock", 10, { y: -250 })
+  .to(".rock", 10, { y: -250 }, "-12")
   .to(".girl", 10, { y: -200 }, "-=10")
-  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
-  .to(".content", 10, { top: "0%" }, "-=10");
+  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=5")
+  // .to(".content", 10, { top: "0%" }, "-=10");
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
